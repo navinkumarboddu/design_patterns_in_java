677e9ad8fc1a4703d67abccdbee725b6aa933507
Navinkumar Boddu <swarmrobots@gmail.com>
2024-04-05 17:04:39 +0530

    added BridgeDemo 2

---
+++ src/main/java/structural/designpatternsinjava/bridge/shape2/Blue.java
@@ -0,0 +1,9 @@
+package structural.designpatternsinjava.bridge.shape2;
+
+public class Blue implements Color {
+
+    @Override
+    public void applyColor() {
+        System.out.println("Applying blue color");
+    }
+}
