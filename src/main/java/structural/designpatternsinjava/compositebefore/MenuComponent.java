6c19f5ee5161da908ab229c573c5ce1b64204ac5
Navinkumar Boddu <swarmrobots@gmail.com>
2024-04-22 23:09:56 +0530

    composite after checkin

--- src/main/java/structural/designpatternsinjava/compositebefore/MenuComponent.java
+++ src/main/java/structural/designpatternsinjava/compositebefore/MenuComponent.java
@@ -6,32 +6,33 @@ import java.util.List;
 public abstract class MenuComponent {
 
     String name;
-
     String url;
-
     List<MenuComponent> menuComponents = new ArrayList<>();
+
+    public MenuComponent add(MenuComponent menuComponent) {
+        throw new UnsupportedOperationException("Feature not implemented at this level");
+    }
+
+    public MenuComponent remove(MenuComponent menuComponent) {
+        throw new UnsupportedOperationException("Feature not implemented at this level");
+    }
 
     public String getName() {
         return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 
     public String getUrl() {
         return url;
     }
 
-    public void setUrl(String url) {
-        this.url = url;
-    }
-
-    public List<MenuComponent> getMenuComponents() {
-        return menuComponents;
-    }
-
     public abstract String toString();
 
+    String print(MenuComponent menuComponent) {
+        StringBuilder builder = new StringBuilder(name);
+        builder.append(": ");
+        builder.append(url);
+        builder.append("\n");
+        return builder.toString();
+    }
 
 }
